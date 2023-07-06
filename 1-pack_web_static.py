2f3efae361b6e1012969f0eb9242b7bc0c1decf2
Eric Eromosele <109639426+6godpro@users.noreply.github.com>
2023-07-06 17:02:25 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,7 +12,7 @@ def do_pack():
     """Creates a .tgz archive file."""
     dt = datetime.strftime(datetime.utcnow(), '%Y%m%d%H%M%S')
     file_path = "versions/web_static_{}.tgz".format(dt)
-    if not os.path.exists('versions'):
+    if not os.path.isdir('versions'):
         if fab.local('mkdir -p versions').return_code != 0:
             return None
     command = "tar -cvzf {} web_static".format(file_path)
