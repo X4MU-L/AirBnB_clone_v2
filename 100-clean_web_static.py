1d1593e47ed42172f16dd8a2bd9e3a7a45d4e1ef
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-07-09 16:14:13 +0100

    (fix) -> refactor code

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 """use fabric to archive web_static files in the current folder"""
 
-from fabric.api import env, run
+from fabric.api import env, run, local
 
 env.hosts = ['18.210.33.133', '35.174.200.184']
 
 
 def do_clean(number=0):
     """"cleans up old versions of archives"""
-    number = 1 if number <= 0 else number
-    cleans = ["versions/", "/data/web_static/releases/"]
-    for dirs_ in cleans:
-        files = run("ls -cltr {dirs_} | awk '{print $NF}'",
-                    hide=True).stdout.split()
+    number = 2 if number <= 0 else number + 1
+    _to_clean = ["versions/", "/data/web_static/releases/"]
+    for dirs_ in _to_clean:
+        files = ""
+        if dirs_ == "versions/":
+            files = local("ls -cltr {dirs_} | awk '{print $NF}'",
+                          hide=True).stdout.split()
+        else:
+            files = run("ls -cltr {dirs_} | awk '{print $NF}'",
+                        hide=True).stdout.split()
         count = 0
         for file in files:
             if run(f"test -f {file} && echo 'True' || echo 'False'", hide=True).stdout.strip() == 'True':
