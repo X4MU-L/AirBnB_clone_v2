aeee33c73f4aacce392d42ffc602bf36bb5a9ea6
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-07-21 00:51:06 +0100

    (feat) -> add /hbnb endpoint to simple flask app

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello_HBNB():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    return "HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
