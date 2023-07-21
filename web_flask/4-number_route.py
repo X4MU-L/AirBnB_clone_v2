22aaa211ff4708446aeed8370871115621e8616c
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-07-21 02:11:27 +0100

    (feat) -> create dybanic route with type value

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,35 @@
+from flask import Flask, g
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello_HBNB():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    return "HBNB!"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    text = escape(text).replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is_cool"):
+    text = escape(text).replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_route(n):
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
