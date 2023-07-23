a253fb50eee9f15f03cb3ca58e002d3488e9ef80
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-07-23 17:16:36 +0100

    (fix) base model

--- models/base_model.py
+++ models/base_model.py
@@ -26,8 +26,7 @@ class BaseModel:
         if kwargs:
             for k, v in kwargs.items():
                 if (k == 'created_at' or k == 'updated_at'):
-                    v = datetime.strptime(v['updated_at'],
-                                          '%Y-%m-%dT%H:%M:%S.%f')
+                    v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
                 if k != '__class__':
                     self.__dict__[k] = v
 
